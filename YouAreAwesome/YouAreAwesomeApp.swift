cdfe4164952a16cc827f3b630eac2191d4a93cbc
weschweitzer@gmail.com <weschweitzer@gmail.com>
2024-09-16 14:23:41 -0400

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Will Schweitzer on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
